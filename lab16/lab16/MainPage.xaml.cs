b73e2991ca10483936e53964bc0ebbeb6a11d10c
Max54611 <magno.mori@tecsup.edu.pe>
2023-12-21 22:23:03 -0500

    Agregar archivos de proyecto.

---
+++ lab16/lab16/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Newtonsoft.Json;
+
+namespace lab16
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class User
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<User>>(content);
+
+                ListDemo.ItemsSource = resultado;
+            }
+
+        }
+    }
+}
